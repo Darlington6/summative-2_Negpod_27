135b94594737a798302b4fe60db250d3e72be5db
nChartine <n.uwase7@gmail.com>
2024-02-20 11:38:43 -0800

    Registering students

--- main.sh
+++ main.sh
@@ -9,3 +9,19 @@ do
         5. Exit application
         (Choose an option between 1-5)"
         read option
+	 if (( $option == 1 ))
+       	 then
+                echo "What is your student email?"
+                read email
+                echo "How old are you?"
+                read age
+                echo "What is your student ID?"
+                read ID
+                if [ -f students-list_1023.txt ]; then
+                        echo $email, $age, $ID >> students-list_1023.txt
+                else
+                        echo  $email, $age, $ID > students-list_1023.txt
+                fi
+	fi
+done
+
