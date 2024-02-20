81a21719613133bf7de3148f88c4f293edc84969
Darlington6 <d.tunyinko@alustudent.com>
2024-02-20 21:30:32 +0200

    Second commit from Desmond - Beginning code of the application

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while true
+do
+        echo "options:
+        1. Registeration
+        2. View all students
+        3. Delete a record
+        4. Update a record
+        5. Exit application
+        (Choose an option between 1-5)"
+        read option
