2e101243d2e748ed7bc4c28c6bb5c45deb63bd47
makito042 <makuochiokoye@gmail.com>
2024-02-20 12:31:42 -0800

    move to directory

---
+++ move-to-directory.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This scripts automatically moves all 4 files to a directory named negpod_27-q1
+mkdir negpod_27-q1
+mv main.sh students-list_1023.txt select-emails.txt student-emails.txt ./negpod_27-q1
