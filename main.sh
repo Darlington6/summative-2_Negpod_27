3a1934a02eaed89a116c062a34048588297b8995
makito042 <makuochiokoye@gmail.com>
2024-02-20 12:06:19 -0800

    update student record

--- main.sh
+++ main.sh
@@ -35,7 +35,21 @@ do
                 echo "Enter the ID to delete"
                 read ID
                 sed -i ", /$ID\$/d" students-list_1023.txt
+ 	elif (( $option == 4 ))
+        then
+                echo "Please enter the student ID"
+                read Student_ID
+                if grep ", $Student_ID$" students-list_1023.txt
+                then
+                        echo "What is your updated student email?"
+                        read updated_email
+                        echo "What is your new age?"
+                        read new_age
+                        old_record=$(grep ", $Student_ID$" students-list_1023.txt)
+                        sed -i "s/$old_record/$(echo $updated_email, $new_age, $Student_ID)/g" students-list_1023.txt
+                else
+                        echo "The provided Student ID is not registered. Please try again"
+		fi
 	fi
-         
 done
 
